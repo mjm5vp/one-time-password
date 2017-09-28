81d53573785db6caf21527c50919ce5b6cf4bf28
Mark Moeller <MarkMoellerUVA@gmail.com>
2017-09-28 16:41:54 -0400

    twilio integration

--- functions/create_user.js
+++ functions/create_user.js
@@ -6,12 +6,12 @@ module.exports = function(req, res) {
     return res.status(422).send({ error: 'bad input' })
   }
   //Format the phone number to remove dashes or parens
-  const phone = String(req.body.phone).replace(/[^\d]/g);
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
 
   //Create new user account with that phone number
-  admin.auth.createUser({ uid: phone })
+  admin.auth().createUser({ uid: phone })
     .then(user => res.send(user))
-    .catch(error => res.status(422.send({ error })))
+    .catch(err => res.status(422).send({ err: error }))
 
   //Respond to that user request saying the account was made
 }
--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const createUser = require('./create_user');
 const serviceAccount = require('./service_account.json');
+const requestOneTimePassword = require('./requestOneTimePassword');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
