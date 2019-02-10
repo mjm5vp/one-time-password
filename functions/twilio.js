bef7e890df2daa12157fcad37dde5ccd3fd8b1f9
Mark Moeller <MarkMoellerUVA@gmail.com>
2019-02-10 16:22:20 -0500

    deleted twilio file

--- functions/twilio.js
+++
@@ -1,6 +0,0 @@
-const twilio = require('twilio');
-
-const accountSid = 'AC539a2446302efe622fce4b22327aaff9';
-const authToken = '4532548f0d821dc75642cd38bf845204';
-
-module.exports = new twilio.Twilio(accountSid, authToken);
